2d5cdbd375205b171a06177726c273f55844f7f0
sumedh ghoderao <sumedhghoderao6825@gmail.com>
2023-11-19 12:47:18 +0530

    Library Manaegment System

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    Library Management System  Designed by <p>ADITYA,SUMEDH,OMKAR,CHATAINYA & SATaYAM</p></a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
